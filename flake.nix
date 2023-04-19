55dee620aa175c9144332b157d0f08ebaea99408
Claudio Ortolina <claudio@pspdfkit.com>
2023-04-19 18:39:22 +0100

    Working Elixir environment

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Robo Clock";
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          # Set the Erlang version
+          erlangVersion = "erlangR25";
+          # Set the Elixir version
+          elixirVersion = "elixir_1_14";
+
+          erlang = pkgs.beam.interpreters.${erlangVersion};
+          elixir = pkgs.beam.packages.${erlangVersion}.${elixirVersion};
+          elixir-ls = pkgs.beam.packages.${erlangVersion}.elixir-ls;
+        in
+        rec {
+          # TODO: Add your Elixir package
+          # packages = flake-utils.lib.flattenTree {
+          # } ;
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              erlang
+              elixir
+              elixir-ls
+            ];
+          };
+        }
+      );
+}
